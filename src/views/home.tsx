1325ff463c8ca8965129d144964da4a25a0a72ed
devchaitanya25 <chaitanyagarg2000@gmail.com>
2024-01-12 16:16:56 +0530

    minor changes

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,8 +1,28 @@
-import React from "react"
+import { FaStar } from "react-icons/fa6";
+
 export const HomePage = () => {
+    const comp=[{name:"facebook",rating:3},{name:"google",rating:5}]
+    const Rating = (value:number) => (
+        <div className="flex flex-row">
+          {[...Array(10)].map((_, index) => (
+            <FaStar
+              key={index}
+              className={index < value ? 'text-yellow-500' : 'text-gray-400'}
+            />
+          ))}
+        </div>
+    )
     return (
-        <div className="text-3xl text-red-300 font-bold underline">
-            Home Page  s
+        <div className="text-3xl text-green-800 font-bold underline h-full w-full flex flex-col items-center">
+            Home Page
+            {comp.map(element =>{ return (
+                <div className="h-24 border-4 border-black rounded w-2/3 text-left p-1 m-1">
+                    {element.name}
+                    {
+                        Rating(element.rating)
+                    }
+                </div>
+            )})}
         </div>
     )
 }
