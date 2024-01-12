2e0aba0e653f86741f375f2312c26ed230f8db38
Mudit Garg <muditgarg87@gmail.com>
2024-01-12 15:28:46 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { HomePage } from './views/home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/home' index element={<HomePage />} />
 
-    <HomePage></HomePage>
-    <div className='text-3xl'>
-    Hello World
-    </div>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
