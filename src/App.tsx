d53ccf0a4ecd16fc2fe7890d658b30dbaa96ebda
devchaitanya25 <chaitanyagarg2000@gmail.com>
2024-01-12 15:10:29 +0530

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { HomePage } from './views/home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+    <HomePage></HomePage>
     <div className='text-3xl'>
-    Ehllo World
+    Hello World
     </div>
     </>
   )
